aca6a05d7d5bd66c4385b6a8f287817dca5391fb
Alexis Haldy <alexishaldy@gmail.com>
2023-05-17 15:12:54 +0200

    Init project

---
+++ build_win.bat
@@ -0,0 +1,11 @@
+mkdir bin
+
+clang++ src/main.cpp ^
+    -o bin/game.exe ^
+    -std=c++14 ^
+    -Wall -Wextra ^
+    -I libs/include ^
+    libs/win-x64/minifb.lib ^
+    -lkernel32 -luser32 -lshell32 -ldxgi -ld3d11 -lole32 -lgdi32 -lwinmm -lopengl32
+
+bin\game.exe
